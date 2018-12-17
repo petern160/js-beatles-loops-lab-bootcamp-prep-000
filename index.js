03f7f16bd2754c6aa58a34a4bfc51bbecc1857d7
Peter Nguyen <peternguyen160@gmail.com>
2018-12-17 06:16:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
+<<<<<<< HEAD
   const array = []
   for (let i = 0, l = musicians.length; i < l; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
@@ -23,4 +24,29 @@ function iLoveTheBeatles(i) {
     i++
   } while (i < 15)
   return array
+=======
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+ function johnLennonFacts() {
+  var facts = [];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  return array;
+}
+ function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number += 1
+  } while (number < 15);
+  return array;
+>>>>>>> d9a29ce125c92c97fdf54a8a58654ee70617415d
 }
